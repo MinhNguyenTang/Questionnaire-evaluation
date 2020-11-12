b3803f2e66e85bfdeb82a6d9f66c9587093246a2
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-12 09:10:38 +0100

    Modification page

--- index.php
+++ index.php
@@ -22,9 +22,9 @@
       <nav class="E2C nav-bar" role="navigation">
         <ul class="link">
           <li><a href="/Questionnaire-evaluation/index.php" class="nav-link">Home</li>
-          <li><a href="/Questionnaire-evaluation/web/.php" class="nav-link">Planning</li>
-          <li><a href="/Questionnaire-evaluation/web/.php" class="nav-link">Mesures</li>
-          <li><a href="/Questionnaire-evaluation/web/.php" class="nav-link">Tutoriels</li>
+          <li><a href="/Questionnaire-evaluation/web/planning.php" class="nav-link">Planning</li>
+          <li><a href="/Questionnaire-evaluation/web/mesures.php" class="nav-link">Mesures</li>
+          <li><a href="/Questionnaire-evaluation/web/tutos.php" class="nav-link">Tutoriels</li>
           <li><a href="/Questionnaire-evaluation/web/contact.php" class="nav-link">Contact</li>
         </ul>
       </nav>
@@ -38,7 +38,19 @@
 <!-- HEADER -->
 
 <body>
+
+  <div class="text-section">
+    <div class="container">
+      <div class="row">
+        <p> Ce site vous permet de vous initier à comment utiliser des outils informatiques pour installer des packages ou un OS (système d"'exploitation')
+        au cas où vous décidez de changer votre manière de travailler. Si vous le souhaitez, veuillez vous rendre à ce lien : <a href="Questionnaire-evaluation/web/tutos.php">Installer un OS avec une clé d'installation</a>
+      </p>
+      </div>
+    </div>
+  </div>
+
   <footer>
   </footer>
+
 </body>
 </html>
