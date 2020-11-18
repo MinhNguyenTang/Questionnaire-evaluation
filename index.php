18bc968f51280da6e979149180ac08c2de680697
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-18 14:19:25 +0100

    Correction page d'accueil

--- index.php
+++ index.php
@@ -7,33 +7,7 @@
 
 <!-- HEAD -->
 
-<header class="" role="banner">
-
-  <div class="container">
-    <div class="row align-items-center position-relative">
-
-      <div class="col-3">
-        <div class="logo">
-          <a href="/Questionnaire-evaluation/index.php"><span class="text-primary">E2C Paris</span></a>
-      </div>
-    </div>
-
-    <div class="col-9 text-right">
-      <nav class="E2C nav-bar" role="navigation">
-        <ul class="link">
-          <li><a href="/Questionnaire-evaluation/index.php" class="nav-link">Home</li>
-          <li><a href="/Questionnaire-evaluation/web/planning.php" class="nav-link">Planning</li>
-          <li><a href="/Questionnaire-evaluation/web/mesures.php" class="nav-link">Mesures</li>
-          <li><a href="/Questionnaire-evaluation/web/tutos.php" class="nav-link">Tutoriels</li>
-          <li><a href="/Questionnaire-evaluation/web/contact.php" class="nav-link">Contact</li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  </div>
-
-
-</header>
+<?php include 'include/header.php'; ?>
 
 <!-- HEADER -->
 
@@ -42,8 +16,8 @@
   <div class="text-section">
     <div class="container">
       <div class="row">
-        <p> Ce site vous permet de vous initier à comment utiliser des outils informatiques pour installer des packages ou un OS (système d"'exploitation')
-        au cas où vous décidez de changer votre manière de travailler. Si vous le souhaitez, veuillez vous rendre à ce lien : <a href="Questionnaire-evaluation/web/tutos.php">Installer un OS avec une clé d'installation</a>
+        <p> Ce site vous permet de vous initier à comment utiliser des outils informatiques pour installer des packages ou un OS (système d'exploitation)
+        au cas où vous décidez de changer votre manière de travailler. Si vous le souhaitez, veuillez vous rendre à ce lien : <a href="/Questionnaire-evaluation/web/tutos.php">Installer un OS avec une clé d'installation</a>
       </p>
       </div>
     </div>
