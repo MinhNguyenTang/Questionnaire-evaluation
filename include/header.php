90b3eca929f56542f03c84e8ee2d8b0d2ce2274a
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-18 14:11:54 +0100

    Correction

--- include/header.php
+++ include/header.php
@@ -14,9 +14,9 @@
     <nav class="E2C nav-bar" role="navigation">
       <ul class="link">
         <li><a href="/Questionnaire-evaluation/index.php" class="nav-link">Home</li>
-        <li><a href="/Questionnaire-evaluation/web/.php" class="nav-link">Planning</li>
-        <li><a href="/Questionnaire-evaluation/web/.php" class="nav-link">Mesures</li>
-        <li><a href="/Questionnaire-evaluation/web/.php" class="nav-link">Tutoriels</li>
+        <li><a href="/Questionnaire-evaluation/web/planning.php" class="nav-link">Planning</li>
+        <li><a href="/Questionnaire-evaluation/web/mesures.php" class="nav-link">Mesures</li>
+        <li><a href="/Questionnaire-evaluation/web/tutos.php" class="nav-link">Tutoriels</li>
         <li><a href="/Questionnaire-evaluation/web/contact.php" class="nav-link">Contact</li>
       </ul>
     </nav>
